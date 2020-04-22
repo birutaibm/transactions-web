1be94b0c939ed9a25d32b20f8d3b199c4cdee6c7
Rafael Arantes <birutaibm@gmail.com>
2020-04-21 21:17:40 -0300

    Finished

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -12,6 +12,15 @@ import formatValue from '../../utils/formatValue';
 
 import { Container, CardContainer, Card, TableContainer } from './styles';
 
+interface TransactionInfo {
+  id: string;
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: string;
+}
+
 interface Transaction {
   id: string;
   title: string;
@@ -30,12 +39,35 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      const { data } = await api.get('transactions');
+      setTransactions(
+        data.transactions.map((transaction: TransactionInfo) => {
+          const date = new Date(transaction.created_at);
+          const day = `0${date.getDate()}`.slice(-2);
+          const month = `0${date.getMonth() + 1}`.slice(-2);
+          const year = date.getFullYear();
+          const formattedDate = `${day}/${month}/${year}`;
+          const sign = transaction.type === 'outcome' ? '- ' : '';
+          const formattedValue = sign + formatValue(transaction.value);
+          return {
+            ...transaction,
+            created_at: date,
+            category: { title: transaction.category.title },
+            formattedValue,
+            formattedDate,
+          };
+        }),
+      );
+      setBalance({
+        income: formatValue(data.balance.income),
+        outcome: formatValue(data.balance.outcome),
+        total: formatValue(data.balance.total),
+      });
     }
 
     loadTransactions();
@@ -51,21 +83,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">{balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">{balance.total}</h1>
           </Card>
         </CardContainer>
 
@@ -81,18 +113,16 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.formattedValue}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.formattedDate}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
